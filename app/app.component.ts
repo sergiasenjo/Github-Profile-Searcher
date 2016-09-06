e802395cabbbcd3560ffd1baa35264d41acca929
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-09-06 16:29:28 +0200

    Profile component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from './components/profile.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><profile></profile>'
 })
 export class AppComponent { }
