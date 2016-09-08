2e7ed71a3a070fdd78e2afe0563467d5e81df864
Sergio Asenjo <sergio.asenjo@novaquality.es>
2016-09-08 15:22:42 +0200

    Code refactor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ProfileComponent } from './components/profile.component';
-import { GithubService } from './services/github.service';
 
 @Component({
     moduleId: module.id,
